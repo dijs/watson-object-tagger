84604f8646b958f2e7b04d929429d7899511d075
Richard van der Dys <richard.vanderdys@gmail.com>
2017-11-27 17:33:43 +0200

    Removed carosuel onyl loading one image at a time.
    Preloading images still not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import Carousel from './Carousel';
+import UntaggedItem from './UntaggedItem';
+import debug from 'debug';
 import './App.css';
+
+const log = debug('Watson-Object-Tagger:App');
+
+function preloadImage(src) {
+  const nextImage = new Image(353, 265);
+  log('Preloading', src);
+  nextImage.src = `${process.env.REACT_APP_API_URL}/untagged/${src}`;
+  nextImage.onload = () => log('Preloaded', src);
+}
 
 class App extends Component {
   constructor(props) {
@@ -8,32 +18,58 @@ class App extends Component {
     this.state = {
       filenames: [],
       labels: [],
-      error: null
+      error: null,
+      currentFileIndex: 0
     };
   }
-  update = () => {
+  init = () => {
     this.setState({ error: null });
     fetch(`${process.env.REACT_APP_API_URL}/untagged`)
       .then(res => res.json())
       .then(filenames => this.setState({ filenames }))
+      .then(() => {
+        preloadImage(this.state.filenames[1]);
+        preloadImage(this.state.filenames[2]);
+        preloadImage(this.state.filenames[3]);
+        return fetch(`${process.env.REACT_APP_API_URL}/recognize/${this.state.filenames[0]}`)
+          .then(res => res.json())
+          .then(({ guess, score }) => this.setState({ guess, score }))
+          .catch(err => this.setState({ error: err.message }));
+      })
       .catch(err => this.setState({ error: err.message }));
     fetch(`${process.env.REACT_APP_API_URL}/labels`)
       .then(res => res.json())
       .then(labels => this.setState({ labels }))
       .catch(err => this.setState({ error: err.message }));
   }
+  next() {
+    const nextState = { error: null, currentFileIndex: this.state.currentFileIndex + 1, guess: null, score: null };
+    if (nextState.currentFileIndex >= this.state.filenames.length) {
+      nextState.filenames = [];
+    } else {
+      preloadImage(this.state.filenames[nextState.currentFileIndex + 1]);
+      preloadImage(this.state.filenames[nextState.currentFileIndex + 2]);
+      preloadImage(this.state.filenames[nextState.currentFileIndex + 3]);
+      fetch(`${process.env.REACT_APP_API_URL}/recognize/${this.state.filenames[nextState.currentFileIndex]}`)
+        .then(res => res.json())
+        .then(({ guess, score }) => this.setState({ guess, score }))
+        .catch(err => this.setState({ error: err.message }));
+    }  
+    this.setState(nextState);
+  }
   onLabel = (filename, label) => {
-    this.setState({ error: null });
+    log('Labeled', filename);
+    this.next();
     fetch(`${process.env.REACT_APP_API_URL}/tag/${filename}/${label}`)
       .catch(err => this.setState({ error: err.message }));
   }
   onNegative = (filename) => {
-    this.setState({ error: null });
+    log('Negative', filename);
+    this.next();
     fetch(`${process.env.REACT_APP_API_URL}/negative/${filename}`)
       .catch(err => this.setState({ error: err.message }));
   }
   onAddLabel = (label) => {
-    this.setState({ error: null });
     fetch(`${process.env.REACT_APP_API_URL}/add-label/${label}`)
       .then(() => fetch(`${process.env.REACT_APP_API_URL}/labels`))
       .then(res => res.json())
@@ -41,17 +77,24 @@ class App extends Component {
       .catch(err => this.setState({ error: err.message }));
   }
   componentDidMount() {
-    this.update();
+    this.init();
   }
   render() {
-    return <Carousel
-      filenames={this.state.filenames}
+    if (this.state.error) {
+      return <div className="alert alert-danger" role="alert"><b>Error:</b> {this.state.error}</div>;
+    }
+    if (!this.state.filenames.length) {
+      return <div className="alert alert-success" role="alert">All tagged, great job!</div>;
+    }
+    return <UntaggedItem
+      filename={this.state.filenames[this.state.currentFileIndex]}
+      guess={this.state.guess}
+      score={this.state.score}
       labels={this.state.labels}
-      error={this.state.error}
       onLabel={this.onLabel}
       onNegative={this.onNegative}
       onAddLabel={this.onAddLabel}
-    />;
+    />
   }
 }
 
--- src/UntaggedItem.js
+++ src/UntaggedItem.js
@@ -1,4 +1,4 @@
-import React,  { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
 import Labels from './Labels';
 import AddLabel from './AddLabel';
@@ -7,26 +7,24 @@ const fullWidth = 640;
 const fullHeight = 480;
 
 function itemTitle(guess, score, label) {
-  if (score > 0.8) {
+  if (guess) {
     return `${guess} with ${Math.round(score * 100)}%`;
   }
   return label;
 }
 
-const apiUrl = process.env.REACT_APP_API_URL || 'http://richard.crushftp.com:5567';
-
-export function UntaggedItem({ filename, tagging, onTag, onLabel, onNegative, labels, onAddLabel, guess, score, onRecognize, processing }) {
+export default function UntaggedItem({ filename, tagging, onTag, onLabel, onNegative, labels, onAddLabel, guess, score, onRecognize, processing, error }) {
   const [label, timestamp, left, right, top, bottom] = filename
     .substring(0, filename.indexOf('.'))
     .split('_');
   const width = right - left;
   const height = bottom - top;
-  const title = itemTitle(guess, score, label);
+  const title = error ? error : itemTitle(guess, score, label);
   return <div className="card">
     <div style={{ position: 'relative' }}>
       <img
         className="card-img-top"
-        src={`${apiUrl}/untagged/${filename}`}
+        src={`${process.env.REACT_APP_API_URL}/untagged/${filename}`}
         alt={label}
       />
       <div style={{
@@ -58,34 +56,4 @@ export function UntaggedItem({ filename, tagging, onTag, onLabel, onNegative, la
       </div>
     </div>
   </div>;
-}
-
-export default class UntaggedItemContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      guess: null,
-      score: 0,
-      processing: false,
-    };
-  }
-  componentDidUpdate() {
-    if (this.props.active && !this.state.processing && !this.state.guess) {
-      this.setState({ processing: true });
-      fetch(`${apiUrl}/recognize/${this.props.filename}`)
-        .then(res => res.json())
-        .then(({ guess, score }) => this.setState({ guess, score, processing: false }))
-        .catch(err => this.setState({ error: err.message, processing: false }));
-    }
-  }
-  render() {
-    return <UntaggedItem
-      {...this.props}
-      processing={this.state.processing}
-      guess={this.state.guess}
-      score={this.state.score}
-      onLabel={this.props.onLabel}
-      onNegative={this.props.onNegative}
-    />;
-  }
-}
+};
